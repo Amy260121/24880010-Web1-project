d09433a03103ee51e1d7c4be0c02848d855be242
Hồ Thị Thảo Dung 24880010 <24880010@student.hcmus.edu.vn>
2026-05-20 01:27:39 +0700

    Delete js/24880010.js

--- js/24880010.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL="https://vfbjppyaucjiknklkpim.supabase.co";
-const SUPABASE_ANON_KEY="sb_publishable_uPEWEl3KNj4OGq_dV27mvA_kpH3e0Lr";
-
-const { createClient } = supabase;
-const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
