c3b80a9ea5c241235171a86b19ef63816a25992a
Hồ Thị Thảo Dung 24880010 <24880010@student.hcmus.edu.vn>
2026-05-23 01:39:48 +0700

    Update database.js

--- js/api/database.js
+++ js/api/database.js
@@ -52,3 +52,87 @@ async function subscribe(email) {
     const {error} = await client.from('subscriptions').insert({email});
     if (error) throw error;
 }
+async function fetchCategoriesByType(type="gallery"){
+    const {data, error} = await client
+        .from('categories')
+        .select()
+        .eq('type', type);
+    if (error){
+        console.error(error);
+        return[];
+    }
+    return data;
+}
+async function fetchGallery(){
+ return fetchDataByTypes(['gallery']);
+}
+async function fetchBlogsByCategoryId(
+    categoryId = "All",
+    page = 1,
+    pageSize = 2,
+){
+    const safeCategoryId = Number(categoryId);
+    const safePage = Math.max(1, Number(page) || 1);
+    const safePageSize = Math.max(1, Number(pageSize) || 2);
+
+    const from = (safePage - 1) * safePageSize;
+    const to = from + safePageSize - 1;
+
+ let query = client
+    .from("information")
+    .select(
+        `
+            id,
+            title,
+            category_id,
+            thumbpath,
+            summary,
+            created_at,
+            author: users!information_author_id_fkey(id, name),
+            category: categories!information_category_id_fkey!inner(id, name, type),
+            comments: comments!comments_blog_id_fkey(id)
+             `,
+        {count: "exact"},// lấy tổng số blog để tính số trang
+    )
+    .eq("category.type", "blogs")
+    .order("created_at", { ascending: false });
+
+
+    if (!Number.isNaN(safeCategoryId) && safeCategoryId > 0){
+        query = query.eq("category_id", safeCategoryId);
+    }
+    const { data, error, count } = await query.range(from, to);
+    const totalItems = count ?? 0;
+    const pageCount = Math.ceil(totalItems / safePageSize);
+    const prevPage =  Math.max(1, safePage - 1);
+    const nextPage = Math.min(pageCount, safePage + 1);
+
+    if (error){
+        console.error(error);
+        return {
+            data: [],
+            pagination: {
+                crurrentPage: safePage,
+                pageCount: 0,
+                category: categoryId,
+                size:2,
+                prevPage,
+                nextPage,
+            },
+            error,
+        };
+    }
+    
+        return {
+            data,
+            pagination: {
+                crurrentPage: safePage,
+                pageCount: 0,
+                category: categoryId,
+                size:2,
+                prevPage,
+                nextPage,
+            },
+            error,
+        };
+}
